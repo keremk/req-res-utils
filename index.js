be6b14871404054a6d93d6fa78241314e14d07c6
Kerem Karatal <kerem.karatal@xing.com>
2018-06-14 13:06:12 +0200

    Fix paging name and CORS naming

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 module.exports.forwardTraceHeaders = require('./lib/tracing');
-module.exports.cors = require('./lib/cors');
+module.exports.getCORSHeaders = require('./lib/cors');
 module.exports.filterItems = require('./lib/filter');
 module.exports.getIds = require('./lib/ids');
-module.exports.getPagingInfo = require('./lib/paging_info');
+module.exports.getPagingInfo = require('./lib/paging');
